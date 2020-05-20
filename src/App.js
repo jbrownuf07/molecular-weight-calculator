cbdd3b07718e793d51b7f037849b3f98b50d3bcc
Joe Brown <jbrownuf07@gmail.com>
2020-05-19 19:18:40 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Container } from 'semantic-ui-react';
+import MolecularWeightCalculatorForm from './components/MolecularWeightCalculatorForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import FormExampleCaptureValues from './components/FormExampleCaptureValues';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <Container>
+        <h1>Molecular Weight Calculator</h1>
+        <MolecularWeightCalculatorForm />
+      </Container>
+    );
+  }
 }
 
 export default App;
